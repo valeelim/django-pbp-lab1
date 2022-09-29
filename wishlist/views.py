82d13efcf097a1b16fbfe8109bfc3368fc7eb5f8
Valee <valeriansalim@gmail.com>
2022-09-29 14:04:11 +0700

    lab

--- wishlist/views.py
+++ wishlist/views.py
@@ -17,7 +17,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Valerian Salim',
+        'username': request.user,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
